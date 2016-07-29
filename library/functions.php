45c1e56a35764946eb08ce13924739131f5f65a5
Juan M <jm@guide42.com>
2016-07-28 22:04:32 -0300

    CS on resource_for

--- library/functions.php
+++ library/functions.php
@@ -6,18 +6,22 @@ namespace Ochenta;
 function resource_of($resource) {
     if (is_null($resource)) {
         return null;
-    } elseif (is_scalar($resource)) {
+    }
+
+    if (is_scalar($resource)) {
         $stream = fopen('php://temp', 'r+');
         if (!empty($resource)) {
             fwrite($stream, $resource);
             fseek($stream, 0);
         }
         return $stream;
-    } elseif (is_resource($resource)) {
-        return $resource;
-    } else {
-        throw new \InvalidArgumentException('Invalid resource');
     }
+
+    if (is_resource($resource)) {
+        return $resource;
+    }
+
+    throw new \InvalidArgumentException('Invalid resource');
 }
 
 /** @throws RuntimeException */
