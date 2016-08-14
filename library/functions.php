73efa4948ade176b513beaa54d157575d8086c89
Juan M <jm@guide42.com>
2016-08-13 23:29:20 -0300

    Move functions around

--- library/functions.php
+++ library/functions.php
@@ -3,83 +3,6 @@
 namespace ochenta;
 
 /** @throws InvalidArgumentException */
-function resource_of($resource) {
-    if (is_null($resource)) {
-        return NULL;
-    }
-
-    if (is_scalar($resource)) {
-        $stream = fopen('php://temp', 'r+');
-        if (!empty($resource)) {
-            fwrite($stream, $resource);
-            fseek($stream, 0);
-        }
-        return $stream;
-    }
-
-    if (is_resource($resource)) {
-        return $resource;
-    }
-
-    throw new \InvalidArgumentException('Invalid resource');
-}
-
-/** @throws RuntimeException */
-function mimetype_of($resource, string $filename=NULL) {
-    $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    if ($finfo === FALSE) {
-        throw new \RuntimeException('Fileinfo database is not available');
-    }
-
-    $mimetype = FALSE;
-    if (is_file($filename) && is_readable($filename)) {
-        $mimetype = finfo_file($finfo, $filename);
-    }
-
-    if ($mimetype === FALSE) {
-        $contents = FALSE;
-
-        if (is_scalar($resource)) {
-            $contents = (string) $resource;
-        } elseif (is_resource($resource)) {
-            $contents = stream_get_contents($resource, -1, 0);
-        }
-
-        if ($contents !== FALSE) {
-            $mimetype = finfo_buffer($finfo, $contents);
-        }
-    }
-    finfo_close($finfo);
-
-    if ($mimetype === FALSE) {
-        throw new \RuntimeException('Couldn\'t detect mime type from resource');
-    }
-
-    return $mimetype;
-}
-
-/** @throws InvalidArgumentException */
-function hash($resource, string $algo='md5') {
-    if (is_scalar($resource)) {
-        return \hash($algo, $resource);
-    }
-
-    if (is_resource($resource)) {
-        if (!stream_get_meta_data($resource)['seekable']) {
-            throw new \InvalidArgumentException('Resource is not hashable (is not seekable)');
-        }
-        $context = hash_init($algo);
-        fseek($resource, 0, SEEK_SET);
-        while (!feof($resource)) {
-            hash_update($context, fread($resource, 4096));
-        }
-        return hash_final($context);
-    }
-
-    throw new \InvalidArgumentException('Resource is not hashable');
-}
-
-/** @throws InvalidArgumentException */
 function responder_of($resource) {
     if ($resource instanceof Response) {
         return function(ServerRequest $req, callable $open) use($resource) {
@@ -218,6 +141,83 @@ function append(string $content, string $tag='body') {
             return $res->getReturn();
         };
     };
+}
+
+/** @throws InvalidArgumentException */
+function resource_of($resource) {
+    if (is_null($resource)) {
+        return NULL;
+    }
+
+    if (is_scalar($resource)) {
+        $stream = fopen('php://temp', 'r+');
+        if (!empty($resource)) {
+            fwrite($stream, $resource);
+            fseek($stream, 0);
+        }
+        return $stream;
+    }
+
+    if (is_resource($resource)) {
+        return $resource;
+    }
+
+    throw new \InvalidArgumentException('Invalid resource');
+}
+
+/** @throws RuntimeException */
+function mimetype_of($resource, string $filename=NULL) {
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    if ($finfo === FALSE) {
+        throw new \RuntimeException('Fileinfo database is not available');
+    }
+
+    $mimetype = FALSE;
+    if (is_file($filename) && is_readable($filename)) {
+        $mimetype = finfo_file($finfo, $filename);
+    }
+
+    if ($mimetype === FALSE) {
+        $contents = FALSE;
+
+        if (is_scalar($resource)) {
+            $contents = (string) $resource;
+        } elseif (is_resource($resource)) {
+            $contents = stream_get_contents($resource, -1, 0);
+        }
+
+        if ($contents !== FALSE) {
+            $mimetype = finfo_buffer($finfo, $contents);
+        }
+    }
+    finfo_close($finfo);
+
+    if ($mimetype === FALSE) {
+        throw new \RuntimeException('Couldn\'t detect mime type from resource');
+    }
+
+    return $mimetype;
+}
+
+/** @throws InvalidArgumentException */
+function hash($resource, string $algo='md5') {
+    if (is_scalar($resource)) {
+        return \hash($algo, $resource);
+    }
+
+    if (is_resource($resource)) {
+        if (!stream_get_meta_data($resource)['seekable']) {
+            throw new \InvalidArgumentException('Resource is not hashable (is not seekable)');
+        }
+        $context = hash_init($algo);
+        fseek($resource, 0, SEEK_SET);
+        while (!feof($resource)) {
+            hash_update($context, fread($resource, 4096));
+        }
+        return hash_final($context);
+    }
+
+    throw new \InvalidArgumentException('Resource is not hashable');
 }
 
 /** @throws InvalidArgumentException */
--- spec/Suite/functionsSpec.php
+++ spec/Suite/functionsSpec.php
@@ -3,79 +3,15 @@
 use Kahlan\Plugin\Monkey;
 use ochenta\ServerRequest;
 use ochenta\Response;
-use function ochenta\resource_of;
-use function ochenta\mimetype_of;
-use function ochenta\hash;
 use function ochenta\emit;
 use function ochenta\responder_of;
 use function ochenta\stack;
 use function ochenta\header;
 use function ochenta\append;
+use function ochenta\resource_of;
+use function ochenta\mimetype_of;
+use function ochenta\hash;
 use function ochenta\escape;
-
-describe('resource_of', function() {
-    it('returns null when null given', function() {
-        expect(resource_of(NULL))->toBeNull();
-    });
-});
-
-describe('mimetype_of', function() {
-    it('throws RuntimeException when null given', function() {
-        expect(function() { mimetype_of(NULL); })->toThrow(new RuntimeException);
-    });
-
-    // Disabled because Monkey::patch doesn't work on included files
-    xit('throws RuntimeException when finfo db is not available', function() {
-        Monkey::patch('finfo_open', function() {
-            return FALSE;
-        });
-
-        expect(function() { mimetype_of('Hello World'); })->toThrow(new RuntimeException);
-    });
-
-    it('returns mimetype from fileinfo db if the file is found', function() {
-        $tmpfile = tempnam(sys_get_temp_dir(), 'kahlan/');
-        skipIf($tmpfile === FALSE);
-        $tmphandle = fopen($tmpfile, 'a');
-        fwrite($tmphandle, 'Hello World');
-        fclose($tmphandle);
-        expect(mimetype_of(NULL, $tmpfile))->toBe('text/plain');
-        unlink($tmpfile);
-    });
-
-    it('returns deduced mimetype of string', function() {
-        expect(mimetype_of('Hello World'))->toBe('text/plain');
-        expect(mimetype_of("\xff\xd8\xffbla.txt"))->toBe('image/jpeg');
-    });
-
-    it('returns deduced mimetype of resource', function() {
-        $resource = fopen('php://temp', 'r+');
-        fwrite($resource, 'Hello World');
-        expect(mimetype_of($resource))->toBe('text/plain');
-        fclose($resource);
-    });
-});
-
-describe('hash', function() {
-    it('throws InvalidArgumentException when null given', function() {
-        expect(function() { hash(NULL); })->toThrow(new InvalidArgumentException);
-    });
-
-    it('throws InvalidArgumentException when the given resource is not seekable', function() {
-        expect(function() { hash(popen('php --version', 'r')); })->toThrow(new InvalidArgumentException);
-    });
-
-    it('returns hash of an scalar', function() {
-        expect(hash(42))->toBe('a1d0c6e83f027327d8461063f4ac58a6');
-        expect(hash('Hello World'))->toBe('b10a8db164e0754105b7a99be72e3fe5');
-    });
-
-    it('returns hash of a resource', function() {
-        $resource = fopen('php://memory', 'r+');
-        fwrite($resource, 'Hello World');
-        expect(hash($resource))->toBe('b10a8db164e0754105b7a99be72e3fe5');
-    });
-});
 
 describe('responder_of', function() {
     it('throws InvalidArgumentException when an invalid resource is given', function() {
@@ -322,6 +258,70 @@ describe('append', function() {
         $midware = append(':)', 'p')($handler);
 
         expect(iterator_to_array($midware(new ServerRequest, function() {}), false))->toBe(['<p>']);
+    });
+});
+
+describe('resource_of', function() {
+    it('returns null when null given', function() {
+        expect(resource_of(NULL))->toBeNull();
+    });
+});
+
+describe('mimetype_of', function() {
+    it('throws RuntimeException when null given', function() {
+        expect(function() { mimetype_of(NULL); })->toThrow(new RuntimeException);
+    });
+
+    // Disabled because Monkey::patch doesn't work on included files
+    xit('throws RuntimeException when finfo db is not available', function() {
+        Monkey::patch('finfo_open', function() {
+            return FALSE;
+        });
+
+        expect(function() { mimetype_of('Hello World'); })->toThrow(new RuntimeException);
+    });
+
+    it('returns mimetype from fileinfo db if the file is found', function() {
+        $tmpfile = tempnam(sys_get_temp_dir(), 'kahlan/');
+        skipIf($tmpfile === FALSE);
+        $tmphandle = fopen($tmpfile, 'a');
+        fwrite($tmphandle, 'Hello World');
+        fclose($tmphandle);
+        expect(mimetype_of(NULL, $tmpfile))->toBe('text/plain');
+        unlink($tmpfile);
+    });
+
+    it('returns deduced mimetype of string', function() {
+        expect(mimetype_of('Hello World'))->toBe('text/plain');
+        expect(mimetype_of("\xff\xd8\xffbla.txt"))->toBe('image/jpeg');
+    });
+
+    it('returns deduced mimetype of resource', function() {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'Hello World');
+        expect(mimetype_of($resource))->toBe('text/plain');
+        fclose($resource);
+    });
+});
+
+describe('hash', function() {
+    it('throws InvalidArgumentException when null given', function() {
+        expect(function() { hash(NULL); })->toThrow(new InvalidArgumentException);
+    });
+
+    it('throws InvalidArgumentException when the given resource is not seekable', function() {
+        expect(function() { hash(popen('php --version', 'r')); })->toThrow(new InvalidArgumentException);
+    });
+
+    it('returns hash of an scalar', function() {
+        expect(hash(42))->toBe('a1d0c6e83f027327d8461063f4ac58a6');
+        expect(hash('Hello World'))->toBe('b10a8db164e0754105b7a99be72e3fe5');
+    });
+
+    it('returns hash of a resource', function() {
+        $resource = fopen('php://memory', 'r+');
+        fwrite($resource, 'Hello World');
+        expect(hash($resource))->toBe('b10a8db164e0754105b7a99be72e3fe5');
     });
 });
 
